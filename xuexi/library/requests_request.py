206b5e462d4f4400929447287267bfb07517681d
Yang Yang <yangyang@we.com>
2016-10-28 01:28:28 +0800

    first requests 慕课！

--- xuexi/library/requests_request.py
+++ xuexi/library/requests_request.py
@@ -2,6 +2,7 @@
 
 import json
 import requests
+from requests import exceptions
 
 URL = 'https://api.github.com'
 
@@ -20,5 +21,48 @@ def request_method():
     print response
     print better_print(response.text)
 
+def params_requests():
+    response = requests.get(build_uri('users'),params={'since':11})
+    print better_print(response.text)
+    print response.request.headers
+    print response.url
+
+def json_requests():
+    response = requests.patch(build_uri('user'), auth=('yy199771','1qazxsw2'),json={'name':'yymmx'})
+    print better_print(response.text)
+    print response.request.headers
+    print response.request.body
+    print response.status_code
+
+def timeout_requests():
+    try:
+        response = requests.get(build_uri('user/emails'),timeout=10)
+        response.raise_for_status()
+    except exceptions.Timeout as e:
+        print e.message
+    except exceptions.HTTPError as e:
+        print e.message
+    else:
+        print response.text
+        print response.status_code
+
+def hard_requests():
+    from requests import Request,Session
+    s = Session()
+    headers = {'User-Agent': 'fake1.3.4'}
+    req = Request('GET',build_uri('user/emails'),auth=('yy199771@163.com','1qazxsw2'),headers=headers)
+    prepped = req.prepare()
+    print prepped.body
+    print prepped.headers
+
+    resq = s.send(prepped, timeout=10)
+    print resq.status_code
+    print resq.request.headers
+    print resq.text
+
 if __name__ == '__main__':
-    request_method()
+    #request_method()
+    #params_requests()
+    #json_requests()
+    #timeout_requests()
+    hard_requests()
