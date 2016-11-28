071287c0d8faae7f1ef01ccee3e27beb78bfe2df
yy199771 <yy199771@163.com>
2016-11-28 20:54:22 +0800

    Merge branches 'dev' and 'master' of https://github.com/yy199771/FirstOne into dev
    # Conflicts:
    #	xuexi/Base/TheFive/testOne.py

--- study/advance/object/homework/twoHomeWork.py
+++ study/advance/object/homework/twoHomeWork.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 '''
 习题：
 定义一个列表的操作类：Listinfo
@@ -22,3 +23,58 @@ list_info = Listinfo([44,222,111,333,454,'sss','333'])
 
 set_info =  Setinfo(你要操作的集合)
 '''
+
+
+class Listinfo(list):
+
+    def __init__(self, l):
+        self.l = l
+
+        if not isinstance(l, list):
+            return '请输入一个list!'
+
+    def add_setinfo(self, keyname):
+
+        self.keyname = keyname
+
+        if (not isinstance(keyname, str)) and (not isinstance(keyname, int)):
+            return '新加入的key不是字符串或者不是整数!'
+
+        self.l.append(keyname)
+        return self.l
+
+    def get_key(self, num):
+        self.num = num
+
+        for x in self.l:
+            if not isinstance(self.num, int):
+                return '请输入一个整数!'
+            if self.num == x:
+                return self.num
+
+    def update_list(self, extendlist):
+        self.extendList = extendlist
+        if not isinstance(self.extendList, list):
+            return '请输入一个list!'
+        # extend 将2个list合并成一个list
+        self.l.extend(self.extendList)
+        return self.l
+
+
+    def del_key(self):
+        # 删除最后一个元素,并返回剩下的最后一个元素
+        self.l.pop()
+        return self.l
+
+list_info = Listinfo([44,222,111,333,454,'sss','333'])
+print type(list_info)
+print list_info.get_key('222')
+print list_info.get_key(44)
+print list_info.add_setinfo('ABC')
+#print list_info.add_setinfo([12,3,2,'3'])
+print list_info.update_list([12,3,2,'3'])
+print list_info.update_list('CDE')
+print list_info.del_key()
+
+
+#class Setinfo():
