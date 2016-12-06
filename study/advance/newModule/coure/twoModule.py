c02ad0f6203ddd5c9434942ca11dcccaa590b89b
Yang Yang <yangyang@we.com>
2016-12-07 00:05:55 +0800

    module第2课

---
+++ study/advance/newModule/coure/twoModule.py
@@ -0,0 +1,36 @@
+#coding=utf-8
+
+'''
+常用模块
+
+1.我们去哪里找模块
+
+python2.7
+
+python3
+
+pypi.python.org python的模块库
+
+2.我们应该首先选择哪些的模块
+
+首先考虑的是，内置模块
+文档：http://docs.python.org/2.7/
+
+3.常用模块
+
+3.1 urllib urllib2 网络
+3.2 datetime time 时间
+3.3 os 系统
+3.4 pickle  对象序列化
+常用数据交换格式 json xml
+
+
+3.5 bsddb key=>value
+3.6 logging 日志
+
+4.他山之石，可以攻玉
+
+print
+
+
+'''
