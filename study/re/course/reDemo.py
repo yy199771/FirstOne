3749167f3148be7a39fa57273000b5c8e32f2c3f
yy199771 <yy199771@163.com>
2016-12-17 01:38:32 +0800

    Merge branches 'dev' and 'master' of https://github.com/yy199771/FirstOne into dev
    # Conflicts:
    #	xuexi/Base/TheFive/testOne.py

--- study/re/course/reDemo.py
+++ study/re/course/reDemo.py
@@ -17,13 +17,32 @@
 + 前面的表达式，出现1到无限次  最少，出现1次
 ? 前面的表达式，出现0到1次  最多，出现1次
 * 前面的表达式，出现0到无限次 出现不出现，都没关系
+$：完全匹配。
+^：以X开头
 
 3.python里的正则模块 re
 4.一些基本操作
-
 
 4.1 一次取配 match:"hello lilei"  r'(\w+) (\w+)'
 4.2 切割 split
 4.3 查找全部 findall
 4.4 finditer 迭代器什么的最有爱了
 '''
+
+import re
+
+# match 匹配
+
+
+
+# 切割
+
+pattern_str = re.compile('\d+')
+pattern_num = re.compile('\d+')
+str = 'a1b2c3d4'
+num = '1a2b3c4d'
+
+re_str = re.split(pattern_str, str)
+print re_str
+re_num = re.split(pattern_num, num)
+print re_num
